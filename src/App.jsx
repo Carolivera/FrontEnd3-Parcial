7c087b4828c3333316d44c1799df46cb1af1faeb
Carolivera <macarolina.olivera@gmail.com>
2023-05-31 17:54:50 -0300

    Preparando el parcial :)

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Card from './Components/Card'
+import Formulario from './Components/Formulario'
+
+function App() {
+   return (
+    <>
+      <div>
+        <Formulario />
+        <Card />
+      </div>
+      
+    </>
+  )
+}
+
+export default App;
---
+++ src/Components/Formulario.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Formulario = ({setBday, bday, setStyles}) => {
+  return (
+    <form>
+      <label >Nombre:</label>
+        <input type="text"/>
+
+        
+    </form>
+  )
+}
+
+export default Formulario;
